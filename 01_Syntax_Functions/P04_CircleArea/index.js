90152252e3a989eb565634597c88ee4dce668682
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-01-29 22:56:23 +0200

    JS

---
+++ 01_Syntax_Functions/P04_CircleArea/index.js
@@ -0,0 +1,21 @@
+function CircleArea(input) {
+    let inputType = typeof(input);
+  let result;
+  if (inputType === "number") {
+  result = Math.pow(input, 2)*Math.PI;
+    console.log(result.toFixed(2));
+  } else console.log(`We can not calculate the circle area, because we receive a ${inputType}).`);
+}
+
+function CircleArea(input){
+    let inputType = typeof(input);
+    let result;
+    if(inputType === "number")
+    {
+        result = Math.pow(input, 2)*Math.PI;
+        console.log(result.toFixed(2));
+    }
+    else console.log(`We can not calculate the circle area, because we receive a ${inputType}.`);
+}
+areaCircle(5);
+areaCircle("name");
