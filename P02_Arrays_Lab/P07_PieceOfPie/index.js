c4237f19a527d195fea03f59202d596c2a860fad
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-02-05 21:32:03 +0200

    JS

---
+++ P02_Arrays_Lab/P07_PieceOfPie/index.js
@@ -0,0 +1,20 @@
+function solve(arr, firstItem, secondItem) {
+
+}
+solve(['Pumpkin Pie',
+'Key Lime Pie',
+'Cherry Pie',
+'Lemon Meringue Pie',
+'Sugar Cream Pie'],
+'Key Lime Pie',
+'Lemon Meringue Pie'
+);
+solve(['Apple Crisp',
+'Mississippi Mud Pie',
+'Pot Pie',
+'Steak and Cheese Pie',
+'Butter Chicken Pie',
+'Smoked Fish Pie'],
+'Pot Pie',
+'Smoked Fish Pie'
+);
