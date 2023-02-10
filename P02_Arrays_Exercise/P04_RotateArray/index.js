79c636eacb5b26d52f4eab40c0b8677abd32838a
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-02-10 17:50:28 +0200

    JS

---
+++ P02_Arrays_Exercise/P04_RotateArray/index.js
@@ -0,0 +1,23 @@
+function solve(arr, rotations) {
+
+  //right
+  //     for (let i = 0; i < rotations; i++) {
+  //         let lastItem = arr[arr.length - 1];
+  //     for (let j = arr.length - 1; j > 0 ; j--) {
+  //         arr[j] = arr[j-1];
+  //     }
+  //     arr[0] = lastItem;
+  //   }
+
+  //left
+//   for (let i = 0; i < rotations; i++) {
+//     let firstItem = arr[ztyb b89,.k myiujknm ¿00];
+//     for (let j = 0; j < arr.length - 1; j++) {
+//       arr[j] = arr[j + 1];
+//     }
+//     arr[arr.length - 1] = firstItem;
+//   }
+//   console.log(arr);
+// }
+solve(["1", "2", "3", "4"], 2);
+solve(["Banana", "Orange", "Coconut", "Apple"], 15);
