6c91d77fb4278a96059580ee87314dbf1c8ffade
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-02-10 15:35:05 +0200

    JS

--- P02_Arrays_Exercise/P02_PrintEveryNthElement/index.js
+++ P02_Arrays_Exercise/P02_PrintEveryNthElement/index.js
@@ -1,9 +1,11 @@
 function solve(input, step) {
-  let arr = [];
-  for (let i = 0; i < input.length; i += step) {
-    arr.push(input[i]);
-  }
-  console.log(arr.join(","));
+//   let arr = [];
+//   for (let i = 0; i < input.length; i += step) {
+//     arr.push(input[i]);
+//   }
+//   console.log(arr.join(","));
+let myArr = input.filter((element, index , input) => { return index % step == 0});
+console.log(myArr.join(","))
 }
 solve(["5", "20", "31", "4", "20"], 2);
 solve(["dsa", "asd", "test", "tset"], 2);
