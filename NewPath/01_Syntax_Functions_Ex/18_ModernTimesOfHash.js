2382f041c7d7ac94999cce876adbdd9397688572
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-02-25 20:45:13 +0200

    JS

--- NewPath/01_Syntax_Functions_Ex/18_ModernTimesOfHash.js
+++ NewPath/01_Syntax_Functions_Ex/18_ModernTimesOfHash.js
@@ -1,19 +1,23 @@
-function solve(text){
-let result = text.split(' ').filter((word) => word.startsWith('#') && word.length > 1 && isValid(word))
-function isValid(word) {
+function solve(text) {
+  let result = text
+    .split(" ")
+    .filter((word) => word.startsWith("#") && word.length > 1 && isValid(word));
+  function isValid(word) {
     let newWord = word.slice(1);
     for (let char of newWord) {
-       char = char.toLowerCase();
-        let currentChar = char.charCodeAt(0)
-        if(!(currentChar >= 97 && currentChar <= 122)) {
-                return false;
-        }
+      char = char.toLowerCase();
+      let currentChar = char.charCodeAt(0);
+      if (!(currentChar >= 97 && currentChar <= 122)) {
+        return false;
+      }
     }
-   return true;
-}
-for (let currentWord of result) {
+    return true;
+  }
+  for (let currentWord of result) {
     console.log(currentWord.slice(1));
+  }
 }
-}
-solve('Nowadays everyone uses # to tag a #special word in #socialMedia');
-solve('The symbol # is known #variously in English-speaking #regions as the #number sign');
+solve("Nowadays everyone uses # to tag a #special word in #socialMedia");
+solve(
+  "The symbol # is known #variously in English-speaking #regions as the #number sign"
+);
