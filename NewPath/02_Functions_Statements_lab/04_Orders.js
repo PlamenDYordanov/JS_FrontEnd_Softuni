822f87af24976f844c813d698fa033dfc6ec57be
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-02-28 13:58:14 +0200

    JS

---
+++ NewPath/02_Functions_Statements_lab/04_Orders.js
@@ -0,0 +1,14 @@
+function solve(type, count) {
+switch(type) {
+    case 'coffee':
+        break;
+        case 'water':
+        break
+        case 'coke':
+        break
+        case 'snacks':
+        break
+}
+}
+solve("water", 5);
+solve("coffee", 2);
