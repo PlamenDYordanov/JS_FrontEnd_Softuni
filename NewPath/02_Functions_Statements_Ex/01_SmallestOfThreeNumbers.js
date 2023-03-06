6d145ec121ce6b5b056ac6b32f6a2819cef036a3
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-06 13:29:07 +0200

    JS

--- NewPath/02_Functions_Statements_Ex/01_SmallestOfThreeNumbers.js
+++ NewPath/02_Functions_Statements_Ex/01_SmallestOfThreeNumbers.js
@@ -1,15 +1,16 @@
-function solve(...parms) {
-  let smallestNum = Number.MAX_SAFE_INTEGER;
-  for (const num of parms) {
-    findBiggestNum(num);
-  }
+function solve(first,second,third) {//...parms) {
+  // let smallestNum = Number.MAX_SAFE_INTEGER;
+  // for (const num of parms) {
+  //   findBiggestNum(num);
+  // }
 
-  function findBiggestNum(number) {
-    if (number < smallestNum) {
-      smallestNum = number;
-    }
-  }
+  // function findBiggestNum(number) {
+  //   if (number < smallestNum) {
+  //     smallestNum = number;
+  //   }
+  // }
+  let smallestNum = Math.min(first,second,third);
   console.log(smallestNum);
 }
 solve(2, 5, 3);
-solve(2, 2, 2);
+solve(2, 2, -2);
--- NewPath/02_Functions_Statements_Ex/02_AddSubtract.js
+++ NewPath/02_Functions_Statements_Ex/02_AddSubtract.js
@@ -1,11 +1,13 @@
 function sum(...numbers) {
-let sum = numbers[0] + numbers[1];
-let subNum = numbers[2]
-function subtract () {
-    return sum - subNum;
-}
-let result =  subtract();
-console.log (result);
+    const sum = (a ,b) => a+b;
+    const subtract = (a,b) => a-b; 
+// let sum = numbers[0] + numbers[1];
+// let subNum = numbers[2]
+// function subtract () {
+//     return sum - subNum;
+// }
+// let result =  subtract();
+console.log (subtract(sum(numbers[0], numbers[1]), numbers[2]));
 }
 
 sum(23, 6, 10);
