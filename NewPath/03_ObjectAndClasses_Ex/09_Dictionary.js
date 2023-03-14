b54c2560b368f6f779e7703b37ffcb93d3922cbe
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-15 00:38:34 +0200

    JS

---
+++ NewPath/03_ObjectAndClasses_Ex/09_Dictionary.js
@@ -0,0 +1,34 @@
+function solve(array) {
+    let dict = {};
+
+    for (let element of array) {
+        let obj = JSON.parse(element);
+        dict = Object.assign(dict, obj);
+    }
+
+    let sortedKeys = Object.keys(dict);
+    sortedKeys.sort();
+
+    for (let term of sortedKeys) {
+        console.log(`Term: ${term} => Definition: ${dict[term]}`);
+    }
+  }
+// for (let value of objArr) {
+//     console.log(`Terms ${value.key()} => Definition: ${objArr[value]}`);
+// }
+
+
+solve([
+  '{"Cup":"A small bowl-shaped container for drinking from, typically having a handle"}',
+  '{"Cake":"An item of soft sweet food made from a mixture of flour, fat, eggs, sugar, and other ingredients, baked and sometimes iced or decorated."} ',
+  '{"Watermelon":"The large fruit of a plant of the gourd family, with smooth green skin, red pulp, and watery juice."} ',
+  '{"Music":"Vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion."} ',
+  '{"Art":"The expression or application of human creative skill and imagination, typically in a visual form such as painting or sculpture, producing works to be appreciated primarily for their beauty or emotional power."} ',
+]);
+solve([
+  '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+  '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+  '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+  '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+  '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}',
+]);
