67fc1c85acd813aa32941711a8bd1e7bec7d1576
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-16 15:24:30 +0200

    JS

--- NewPath/04_DomTree_Lab/01. Sum-Numbers/SumNumbers.js
+++ NewPath/04_DomTree_Lab/01. Sum-Numbers/SumNumbers.js
@@ -1,3 +1,11 @@
 function calc() {
-    // TODO: sum = num1 + num2
+ const firstInput = document.getElementById('num1');
+ const secondInput = document.getElementById('num2');
+ let result = document.getElementById('sum');
+
+ let fistNum = Number(firstInput.value);
+ let secondNum = Number(secondInput.value);
+ let sum = fistNum + secondNum;
+ result.value = sum;
+ 
 }
