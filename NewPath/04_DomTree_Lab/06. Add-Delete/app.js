4a29ca09768526980085835fe2124351a78d9269
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-16 22:16:07 +0200

    JS

--- NewPath/04_DomTree_Lab/06. Add-Delete/app.js
+++ NewPath/04_DomTree_Lab/06. Add-Delete/app.js
@@ -1,3 +1,18 @@
 function addItem() {
-    //TODO...
+    let input = document.getElementById('newItemText');
+    let newAnchor = document.createElement('a');
+    newAnchor.setAttribute('href', '#');
+    let newLi = document.createElement('li');
+    let container = document.getElementById('items');
+    newLi.textContent = input.value;
+    newLi.appendChild(newAnchor);
+    newAnchor.addEventListener('click',deleteHandler);
+    container.appendChild(newLi);
+    newAnchor.textContent = '[Delete]'
+    input.value = '';
+
+    function deleteHandler(e) {
+       const liItem = e.currentTarget.parentElement;
+       liItem.remove();
+    } 
 }
