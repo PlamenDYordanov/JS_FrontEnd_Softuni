644e3c4a8f44d5038397de9409f1e52f43fe9b5d
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-16 21:55:53 +0200

    JS

--- NewPath/04_DomTree_Lab/05. Delete-From-Table/app.js
+++ NewPath/04_DomTree_Lab/05. Delete-From-Table/app.js
@@ -1,3 +1,13 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  let result = document.getElementById("result");
+  let input = document.querySelector("input[name=email]");
+  let emails = Array.from(document.querySelectorAll("td:nth-child(even)"));
+  let emailValue = input.value;
+  let foundElement = emails.find((email) => email.textContent === emailValue);
+  if (foundElement) {
+    foundElement.parentElement.remove();
+    result.textContent = "Delete.";
+  } else {
+    result.textContent = "Not found.";
+  }
 }
