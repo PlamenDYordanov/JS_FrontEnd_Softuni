846e9f886056b91358e65242d4ab4da450413fe9
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-16 23:01:37 +0200

    JS

--- NewPath/04_DomTree_Lab/08. Sum-Table/SumTable.js
+++ NewPath/04_DomTree_Lab/08. Sum-Table/SumTable.js
@@ -1,3 +1,9 @@
 function sumTable() {
-
-}
+    let prices = Array.from(document.querySelectorAll('tr> td:nth-child(even)'));
+    let result = document.getElementById('sum');
+    let sum = 0
+    for (const price of prices) {
+       sum += Number(price.textContent)
+    }
+    result.textContent = sum;
+    }
