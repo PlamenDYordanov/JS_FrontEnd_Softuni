ef42160c7f5fc76c7f0b9241c3059d8a88d5bc51
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-18 17:46:48 +0200

    JS

--- NewPath/04_DomTree_Ex/06. Table - Search Engine/solution.js
+++ NewPath/04_DomTree_Ex/06. Table - Search Engine/solution.js
@@ -1,8 +1,17 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
-
-   }
+  function onClick() {
+    let tableSelector = Array.from(
+      document.querySelectorAll("body > table > tbody > tr")
+    );
+    const input = document.querySelector("#searchField");
+    for (const row of tableSelector) {
+      if (row.textContent.includes(input.value)) {
+        row.classList.add('select')
+      }else {
+         row.classList.remove('select');
+      }
+    }
+  }
 }
