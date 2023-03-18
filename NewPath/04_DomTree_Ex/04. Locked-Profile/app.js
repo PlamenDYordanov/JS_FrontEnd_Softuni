075447247f55f20adeb64b47e227538c28dfdf59
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-18 16:44:50 +0200

    JS

--- NewPath/04_DomTree_Ex/04. Locked-Profile/app.js
+++ NewPath/04_DomTree_Ex/04. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+  let getAllProfiles = Array.from(document.getElementsByClassName("profile"));
+  for (let profile of getAllProfiles) {
+    let button = profile.getElementsByTagName("button")[0];
+    button.addEventListener("click", clickHandler);
+  }
+  function clickHandler(e) {
+    let currentButton = e.currentTarget;
+    let takeParent = currentButton.parentElement;
+    let getHiddenInfo = takeParent.getElementsByTagName("div")[0];
+    const radioButton = takeParent.querySelector(":nth-child(3)");
+    if (!radioButton.checked) {
+      if (currentButton.textContent === "Show more") {
+        getHiddenInfo.style.display = "block";
+        currentButton.textContent = "Hide it";
+      } else {
+        getHiddenInfo.style.display = "none";
+        currentButton.textContent = "Show more";
+      }
+    }
+  }
 }
--- NewPath/04_DomTree_Ex/05. Fill-Dropdown/dropdown.js
+++ NewPath/04_DomTree_Ex/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
-function addItem() {
-    console.log('TODO:...');
+function addItem() {debugger
+    let inputText = document.getElementById('newItemText');
+    let valueText = document.getElementById('newItemValue');
+    let menu = document.getElementById('menu');
+    let createOption = document.createElement('option');
+    createOption.value = valueText.value;
+    createOption.textContent = inputText.value;
+    menu.appendChild(createOption);
+    inputText.value = '';
+    valueText.value = '';
 }
