405c17f98e02337c0db1b3c0c754817cd3a2090b
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-18 13:00:51 +0200

    JS

--- NewPath/04_DomTree_Lab/09. Highlight-Active/app.js
+++ NewPath/04_DomTree_Lab/09. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focused() {
-    console.log('TODO:...');
+  let inputs = Array.from(document.getElementsByTagName("input"));
+
+  for (let input of inputs) {
+    input.addEventListener("focus", hover);
+    input.addEventListener("blur", unHover);
+  }
+  function hover(e) {
+    let item = e.currentTarget.parentElement;
+    item.style.background = "grey";
+    item.setAttribute("class", "focused");
+  }
+  function unHover(e) {
+    let item = e.currentTarget.parentElement;
+    item.style.background = "white";
+    item.removeAttribute("class");
+  }
 }
