f59984eb9c2cfb8ed6eca929efe5d2b6bc6124fb
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-18 15:20:31 +0200

    JS

--- NewPath/04_DomTree_Ex/02. Sections/script.js
+++ NewPath/04_DomTree_Ex/02. Sections/script.js
@@ -1,3 +1,23 @@
 function create(words) {
-   console.log('TODO:...');
+  let container = document.getElementById("content");
+
+  for (let i = 0; i < words.length; i++) {
+    let createdDiv = document.createElement("div");
+    let createdParagraph = document.createElement("p");
+    createdParagraph.textContent = `${words[i]}`;
+    createdDiv.appendChild(createdParagraph);
+    createdParagraph.style.display = "none";
+    container.appendChild(createdDiv);
+    createdDiv.addEventListener("click", (event) => {
+      let currentDiv = event.currentTarget;
+      let currentParagraph = currentDiv.getElementsByTagName("p")[0];
+      currentParagraph.style.display = "block";
+    });
+
+   //  function clickHandler(e) {
+   //    let currentDiv = e.currentTarget;
+   //    let currentParagraph = currentDiv.getElementsByTagName("p")[0];
+   //    currentParagraph.style.display = "block";
+   //  }
+  }
 }
--- NewPath/04_DomTree_Ex/03. Accordion/accordion.js
+++ NewPath/04_DomTree_Ex/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+  let button = document.getElementsByClassName("button")[0];
+  let additionalText = document.getElementById("extra");
+  button.textContent === "More" ? show() : hide();
+
+  function show() {
+    additionalText.style.display = "block";
+    button.textContent = "Less";
+  }
+  function hide() {
+    additionalText.style.display = 'none';
+    button.textContent  = 'More';
+  }
 }
