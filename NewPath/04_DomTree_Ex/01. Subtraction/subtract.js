fa396d13ec0cffa674a69feb0edc4e7216f65030
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-18 13:15:08 +0200

    JS

--- NewPath/04_DomTree_Ex/01. Subtraction/subtract.js
+++ NewPath/04_DomTree_Ex/01. Subtraction/subtract.js
@@ -1,3 +1,7 @@
 function subtract() {
-    console.log('TODO:...');
+    const firstNumber = Number(document.getElementById('firstNumber').value);
+    const secondNumber = Number(document.getElementById('secondNumber').value);
+   let result = document.getElementById('result');
+   debugger;
+   result.textContent = firstNumber - secondNumber;
 }
