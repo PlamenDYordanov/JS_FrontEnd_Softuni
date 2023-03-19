722d99b50df875fc740e20893764f1f7ae1d9e6e
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-19 15:53:04 +0200

    JS

--- NewPath/04_DomTree_Ex/07. Format the Text/solution.js
+++ NewPath/04_DomTree_Ex/07. Format the Text/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  //TODO
+  let inputText = document.getElementById("input");
+  let output = document.getElementById("output");
+  let inputToArray = inputText.value.split(".");
+  inputToArray.splice(inputToArray.length - 1);
+  while (inputToArray.length > 0) {
+    let firstThreeSentence = inputToArray
+      .splice(0, 3)
+      .map((p) => p.trimStart());
+    let createParagraph = document.createElement("p");
+    createParagraph.textContent = firstThreeSentence.join(".") + ".";
+    output.appendChild(createParagraph);
+  }
 }
