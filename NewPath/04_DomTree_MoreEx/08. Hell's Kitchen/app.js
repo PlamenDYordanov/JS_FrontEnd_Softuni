fa03c8961eb74dfa9b4f218c7220e9e7d6c35013
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-22 15:44:20 +0200

    JS

--- NewPath/04_DomTree_MoreEx/08. Hell's Kitchen/app.js
+++ NewPath/04_DomTree_MoreEx/08. Hell's Kitchen/app.js
@@ -1,8 +1,61 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+  document.querySelector("#btnSend").addEventListener("click", onClick);
 
-   function onClick () {
-      //   TODO:
-      
-   }
+  function onClick() {
+    let restaurant = {};
+    let input = document.querySelector("#inputs textarea");
+    let bestRestaurantOutput = document.querySelector('#bestRestaurant p');
+    let bestWorkersOutput = document.querySelector('#workers p');
+    let newInput = JSON.parse(input.value);
+    for (let index = 0; index < newInput.length; index++) {
+      let [restaurantName, workers] = newInput[index].split(" - ");
+      let splitInput = newInput[index].split("-");
+      let nameOfRestaurant = splitInput[0];
+      let arrayOfWorkers = [];
+      workers = workers.split(" ");
+      for (let i = 0; i < workers.length; i += 2) {
+        let nameOfWorker = workers[i];
+        let currentSalary = Number(workers[i + 1].split(",")[0]);
+        if (!restaurant.hasOwnProperty(restaurantName)) {
+          restaurant[restaurantName] = [];
+        }
+        restaurant[restaurantName].push({
+          name: nameOfWorker,
+          salary: currentSalary,
+        });
+      }
+      const currentRestaurantValues = Object.values(restaurant[restaurantName]);
+      let size = currentRestaurantValues.length;
+      let sum = 0;
+      for (const worker of currentRestaurantValues) {
+        let type = typeof worker.salary;
+        if (type === "number") {
+          sum += worker.salary;
+        }else {
+          size--
+        }
+      }
+      restaurant[restaurantName].average = (sum / size).toFixed(2);
+    }
+    const values = Object.values(restaurant);
+    let bestAverage = values.sort((a, b) => b.average - a.average)[0];
+    let bestSalary = bestAverage.sort((a, b) => b.salary - a.salary)[0];
+    const entries = Object.entries(restaurant);
+    let name = entries.sort((a, b) => b[1].average - a[1].average)[0][0];
+    let average = bestAverage.average;
+    let highestSalary = Number(bestSalary.salary).toFixed(2);
+    const outputBestRestaurant = `Name: ${name} Average Salary: ${average} Best Salary: ${highestSalary}`;
+    let outputBestWorkers = [];
+    for (const key in bestAverage) {
+      let type = typeof bestAverage[key].salary;
+      if (type === "number") {
+        const currentName = bestAverage[key].name;
+        const currentSalary = Number(bestAverage[key].salary).toFixed(0);
+        let currentResult = `Name: ${currentName} With Salary: ${currentSalary}`;
+        outputBestWorkers.push(currentResult);
+      }
+    }
+    bestRestaurantOutput.textContent = outputBestRestaurant;
+    bestWorkersOutput.textContent = outputBestWorkers.join(' ');
+  }
 }
--- NewPath/04_DomTree_MoreEx/09. Generate Report/report.js
+++ NewPath/04_DomTree_MoreEx/09. Generate Report/report.js
@@ -1,3 +1,22 @@
 function generateReport() {
-    //TODO
+  const getHeaderOfTable = Array.from(document.querySelector("body > main > table > thead > tr").children);
+  const getTableBody = Array.from(document.querySelector("body > main > table > tbody").children);
+  const textAreaOutput = document.querySelector('#output');
+  let outputObject = [];
+  for (const currentEmployee of getTableBody) {
+    let index = 0;
+    let arrayOfColumn = Array.from(currentEmployee.children);
+    let currentObject = {}
+    for (const currentRow of getHeaderOfTable) {
+        let inputStats = currentRow.querySelector("input");
+      if (inputStats.checked) {
+          let name = inputStats.name;
+          currentObject[name] = arrayOfColumn[index].textContent
+        }
+        index++;
+    }
+    outputObject.push(currentObject) 
+  }
+  let output = JSON.stringify(outputObject);
+  textAreaOutput.value = output;
 }
