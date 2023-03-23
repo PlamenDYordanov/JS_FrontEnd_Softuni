e5ece62b64eff05a5e80c77594934e981c318368
Plamen Yordanov <plamenyordanov91@gmail.com>
2023-03-23 11:03:12 +0200

    JS

--- NewPath/05_HTTP_Rest_Lab/05. Github-Repos-by-Username/scripts/app.js
+++ NewPath/05_HTTP_Rest_Lab/05. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,29 @@
-function loadRepos() {
-	console.log("TODO...");
+async function loadRepos() {
+	//let newAnchor = document.createElement('a');
+    //newAnchor.setAttribute('href', '#');
+	
+	const BASE_URL = 'https://api.github.com/users/';
+	const username = document.getElementById('username');
+	const usernameValue = username.value;
+	const currentUl = document.getElementById('repos');
+	const exampleLi = document.querySelector('#repos > li').remove();
+
+	try{
+		const currentUser = await fetch(`${BASE_URL}${usernameValue}/repos`);
+		const data = await currentUser.json();
+		data
+		.forEach((repoInfo) => {
+			const li = document.createElement('li');
+			const anchor = document.createElement('a');
+			anchor.setAttribute('href', repoInfo.html_url);
+			anchor.setAttribute('target', '_blank');
+			anchor.textContent = `${repoInfo.full_name}`;
+			li.appendChild(anchor);
+			currentUl.appendChild(li);
+		})
+	}catch(err){
+		const li = document.createElement('li');
+		li.textContent = err;
+		currentUl.appendChild(li);
+	}
 }
--- NewPath/05_HTTP_Rest_Lab/06. Github-Commits/app.js
+++ NewPath/05_HTTP_Rest_Lab/06. Github-Commits/app.js
@@ -1,4 +1,36 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    const BASE_URL = 'https://api.github.com/repos/';
+    const username = document.getElementById('username');
+    const repo = document.getElementById('repo');
+    const commits = document.getElementById('commits');
+    const usernameVal = username.value;
+    const repoVal = repo.value;
+    try{
+        const allCommitsResult = await fetch(`${BASE_URL}${usernameVal}/${repoVal}/commits` )
+        const data = await allCommitsResult.json();
+        data
+        .forEach(({commit}) => {
+                const createLi =document.createElement('li');
+                createLi.textContent = `${commit.author.name}: ${commit.message}`;
+                commits.appendChild(createLi);
+        })
+    }catch(err){
+        const createLi =document.createElement('li');
+       createLi.textContent = err.message
+       commits.appendChild(createLi);
+    }
+    //  fetch(`${BASE_URL}${usernameVal}/${repoVal}/commits`)
+    //  .then((res) => res.json())
+    //  .then((data) => {
+    //     data
+    //     .forEach(({commit}) => {
+    //             const createLi =document.createElement('li');
+    //             createLi.textContent = `${commit.author.name}: ${commit.message}`;
+    //             commits.appendChild(createLi);
+    //     })
+    //  })
+    //  .catch((err) => {
+    //     console.log(err);
+    //  })
+    // console.log('TODO...');
 }
